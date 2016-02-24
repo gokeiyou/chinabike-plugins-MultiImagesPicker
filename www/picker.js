c7f3aece894815e48fefec41631f4b0bdd3ebc0c
Jimmy Wu <wjy307@qq.com>
2016-02-24 17:14:32 +0800

    Create By 2016-2-24 17:13:42
    MultiImagesPicker Plugin Create

---
+++ www/picker.js
@@ -0,0 +1,14 @@
+cordova.define("chinabike-plugins-MultiImagesPicker.MultiImagesPicker", function(require, exports, module) {
+    var exec = require('cordova/exec');
+
+    module.exports = {
+        /**
+         * @param onSuccess 图片选取成功的回调函数
+         * @param onFail 失败的回调函数
+         * @param params 可选参数：{max:最多选取几张图片(默认9张),width:图片宽度,height:图片高度,quality:图片质量}
+         */
+        getPictures: function(onSuccess, onFail, params) {
+            exec(onSuccess, onFail, "MultiImagesPicker", "getPictures", [params]);
+        }
+    }
+});
